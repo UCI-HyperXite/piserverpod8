50b31354bbcea551cc19ba9fd1feea9c26221adb
rscholin <ryescholin@gmail.com>
2023-05-06 14:52:24 -0700

    helppleasegodplease

---
+++ HX_FSM_code.py
@@ -0,0 +1,53 @@
+from app import *
+from enum import Enum 
+import uvicorn
+
+
+class STATE(Enum):
+    LOADING = 1
+    STARTING = 2
+    RUNNING = 3
+    STOPPING = 4
+    STOPPED = 5
+
+STATE = Enum('STATE', ['LOADING', 'STARTING', 'RUNNING', 'STOPPING', 'STOPPED' ])
+
+current_state = 1
+
+bms_values = 0.0
+PT1_value = 0.0
+PT2_value = 0.0
+PT3_value = 0.0
+GUI_message = 0.0
+brakes_signalled = 0.0
+power_check = 0.0 
+
+def main():
+    uvicorn.run(
+        "app:app",
+        port=8000,
+        log_level="info",
+        access_log=False,
+        use_colors=True,
+        reload=True,
+    )
+    STATE = 1
+    while(True):
+        #-------???----------------EPR_COM_CHECK
+        #-------???----------------BMS_COM_CHECK
+        #-------???----------------GUI_CONNECTED
+
+        if (current_state == 1):
+            #CONTACTOR OFF----------------
+            #BRAKES NOT ACTUATED----------
+            state1_message = "Brakes are not actuated \n Contactor turned off"
+            print("BOOOOOO Brakes are not actuated \n Contactor turned off")
+            state1(state1_message)
+       
+           
+if __name__ == "__main__":
+    
+    
+    main()
+    
+           
--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import socketio
+import uvicorn
+#from index import PiServer
+ 
    
 sio = socketio.AsyncServer(
     async_mode="asgi", cors_allowed_origins="http://localhost:3000"
 )
 
+#sio.register_namespace("/pi")
+
 app = socketio.ASGIApp(sio)
+
+buttonSignal = 0;
+old_bms = 0;
 
 @sio.event
 async def connect(sid, environ):
@@ -22,19 +30,23 @@ async def sum(sid, data):
     await sio.emit('sum_result', {'result': result}, to=sid)
 
 
-
 @sio.event
 async def start(sid, data):
-    if data == "start":
-        await sio.emit('starting pod', "Stage 3: Pod is starting", to=sid)
+    if data == 2:
+        buttonSignal = 0
+        buttonSignal += data
+        await sio.emit('starting pod', "Stage 2: Pod is starting", to=sid)
 
 @sio.event
 async def stop(sid, data):
-    if data == "stop":
-        await sio.emit('stopping pod', "Stage 5: Pod is stopping", to=sid)
+    if data == 4:
+        buttonSignal = 0
+        buttonSignal += data
+        await sio.emit('stopping pod', "Stage 4: Pod is stopping", to=sid)
 
-@sio.event
-async def fstop(sid, data):
-    if data == "fstop":
-        await sio.emit('fstopping pod', "FORCE STOPPING POD", to=sid)
+def state1(msg):
+    @sio.event
+    async def state1(sid):
+        sio.emit('Loading State', msg, to=sid)
+
 
