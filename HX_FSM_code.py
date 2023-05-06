5f83e10c55af7abe070c9d2eb8e392a0e48eba44
sm-sm-dot <74443337+sm-sm-dot@users.noreply.github.com>
2023-05-06 14:58:08 -0700

    Update HX_FSM_code.py
    Threading - test state 1

--- HX_FSM_code.py
+++ HX_FSM_code.py
@@ -3,27 +3,28 @@ from app import *
 from enum import Enum 
 import uvicorn
 
+import threading
 
-class STATE(Enum):
-    LOADING = 1
-    STARTING = 2
-    RUNNING = 3
-    STOPPING = 4
-    STOPPED = 5
+def run_loop():
+    global current_state
+    while True:
+        #-------???----------------EPR_COM_CHECK
+        #-------???----------------BMS_COM_CHECK
+        #-------???----------------GUI_CONNECTED
 
-STATE = Enum('STATE', ['LOADING', 'STARTING', 'RUNNING', 'STOPPING', 'STOPPED' ])
-
-current_state = 1
-
-bms_values = 0.0
-PT1_value = 0.0
-PT2_value = 0.0
-PT3_value = 0.0
-GUI_message = 0.0
-brakes_signalled = 0.0
-power_check = 0.0 
+        if current_state == 1:
+            #CONTACTOR OFF----------------
+            #BRAKES NOT ACTUATED----------
+            state1_message = "Brakes are not actuated \n Contactor turned off"
+            print("BOOOOOO Brakes are not actuated \n Contactor turned off")
+            state1(state1_message)
 
 def main():
+    # start the loop in a separate thread
+    loop_thread = threading.Thread(target=run_loop)
+    loop_thread.start()
+
+    # start the web server in the main thread
     uvicorn.run(
         "app:app",
         port=8000,
@@ -32,19 +33,6 @@ def main():
         use_colors=True,
         reload=True,
     )
-    STATE = 1
-    while(True):
-        #-------???----------------EPR_COM_CHECK
-        #-------???----------------BMS_COM_CHECK
-        #-------???----------------GUI_CONNECTED
-
-        if (current_state == 1):
-            #CONTACTOR OFF----------------
-            #BRAKES NOT ACTUATED----------
-            state1_message = "Brakes are not actuated \n Contactor turned off"
-            print("BOOOOOO Brakes are not actuated \n Contactor turned off")
-            state1(state1_message)
-       
            
 if __name__ == "__main__":
     
