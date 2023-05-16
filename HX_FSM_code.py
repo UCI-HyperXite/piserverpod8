9378ddf80346438a673373abd20465f72ec41279
sm-sm-dot <74443337+sm-sm-dot@users.noreply.github.com>
2023-05-16 15:13:29 -0700

    Update HX_FSM_code.py
    use with gather

--- HX_FSM_code.py
+++ HX_FSM_code.py
@@ -1,46 +1,56 @@
-from app import sio, get_sid_save
-from enum import Enum 
-import uvicorn
-import asyncio
-import time
+    while(True):
+        #LOADING----------------------------------------------------------------  
+        if (current_state == 1):
+            state1_message = "Brakes are not actuated \n Contactor turned off"
+            print("Brakes are not actuated \n Contactor turned off")
+            state1(state1_message)
+            if(PT1_value == 1):
+                print("PT 1 Checked!")
+            else:
+                print("PT 1  Failed!")
+            if(PT2_value == 1):
+                print("PT 2 Checked!")
+            else:
+                print("PT 2  Failed!")
+            if(BMS_value < 3.4):
+                print("Error in BMS values!")
+                #disconnect battery from motors
+            else:
+                print("Log all cell values")
+            #GUI Indicates that we want to run the pod then:
+            if (GUI_message == 2):
+                current_state = 2
+                await sio.emit("heard_message", 'heard!', to=sid_save_value)
+            else:
+                print("GUI has not indicated start still!")
+               
+        #STARTING----------------------------------------------------------------
+        if(current_state == 2):
+            #SIGNAL THE BRAKES
+            #SEND DATA to GUI 
+            await sio.emit("PT1_value", PT1_value, to=sid_save_value)
+            await sio.emit("PT2_value", PT2_value, to=sid_save_value)
+            await sio.emit("BMS_value", BMS_value, to=sid_save_value)
+            if(brakes_signalled == 1):
+                print("t\The brakes have been signalled successfully")
+            else:
+                print("The brakes were not signalled successfully")
 
-async def run_loop() -> None:
-    current_state = 1
-    while True:
-        #-------???----------------EPR_COM_CHECK
-        #-------???----------------BMS_COM_CHECK
-        #-------???----------------GUI_CONNECTED
-        try:
-            if current_state == 1:
-                #CONTACTOR OFF----------------
-                #BRAKES NOT ACTUATED----------
-                state1_message = "Brakes are not actuated \nContactor turned off"
-                print("BOOOOOO Brakes are not actuated \n Contactor turned off")
-                sid_save_value = get_sid_save()
-                sio.emit('sum_result', state1_message, broadcast=True)
-                print(sid_save_value)       
-            await asyncio.sleep(1)
-        except KeyboardInterrupt:
-            # currently not working :(
-            break
-        
+        #RUNNING--------------------------------------------------------------------
+        if(current_state == 3):
+            await sio.emit("PT1_value", PT1_value, to=sid_save_value)
+            await sio.emit("PT2_value", PT2_value, to=sid_save_value)
+            await sio.emit("BMS_value", BMS_value, to=sid_save_value)
+            if (GUI_message == 4):
+                print("GUI requested STOP")
+                current_state = 4
 
-
-config = uvicorn.Config(
-    "app:app",
-    port=8000,
-    log_level="info",
-    access_log=True,
-    use_colors=True,
-    reload=True,
-)
-server = uvicorn.Server(config)
-
-
-async def main() -> None:
-    """Run Uvicorn server and FSM concurrently"""
-    await asyncio.gather(server.serve(), run_loop(), return_exceptions=True)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+            if(power_check == 1):
+                print("Power check successful! ")
+            else:
+                print("Power check failed")
+            current_state = 4
+            
+        #STOPPING-------------------------------------------------------------------------
+        if(current_state == 4):
+            #STOP
