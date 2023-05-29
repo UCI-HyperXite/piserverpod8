a4e7f96ff44c0aa1c0e49715e52def428ab9a2ce
ryescholin <46325761+ryescholin@users.noreply.github.com>
2023-05-29 12:06:49 -0700

    Update run_loop.py

--- run_loop.py
+++ run_loop.py
@@ -1,9 +1,11 @@
 # run_loop.py
 from app import sio
-from utils import get_sid_save, get_current_state, set_current_state, get_v
-from tester import main as mainTest
+from utils import get_sid_save, get_current_state, set_current_state, get_v, get_break_state
 import asyncio
-#from basic_braking_rpi import #break_start, #break_stop
+import RPi.GPIO as GPIO
+
+# from pt_test import pt300
+#from basic_braking_rpi import break_start, break_stop
 #from current import vplus, vminus, shunt_current
 #from podmotor import motor_start, motor_stop, get_motor_data
 #from bms import read_bms
@@ -15,15 +17,14 @@ PT_5000 = 0.0
 PT_300 = 0.0
 PT3_value = 0.0
 GUI_message = 0.0
-brakes_signalled = 0.0
+brakes_signalled = 0
 power_check = 0.0
+calc = 0
 
 
 async def send_data():
     sid_save_value  = get_sid_save()
-    asyncio.run(mainTest())
-
-    await sio.emit("pressure300", data, to=sid_save_value)
+#     await sio.emit("pressure300", pt300(), to=sid_save_value)
 
     #bms = read_bms()
     #BMS_value = bms["highest_cell_voltage"]
@@ -39,6 +40,8 @@ async def send_data():
     #await sio.emit("Distance", distance, to=sid_save_value)
 
 async def run_loop() -> None:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(26, GPIO.OUT)
     while True:
         try:
             if (get_current_state() == 1):
@@ -48,14 +51,14 @@ async def run_loop() -> None:
                 print("CURRENT STATE 1", get_current_state())
                 state1_message = "Brakes are not actuated \n Contactor turned off"
                 print("Brakes are not actuated \n Contactor turned off")
-                print("vvalue:", get_v())
                 sid_save_value  = get_sid_save()
                 await sio.emit("state1", state1_message, to=sid_save_value)
-                await sio.emit("state1", return_data, to=sid_save_value)
+#                 await sio.emit("pressure300", pt300(), to=sid_save_value)
 
                 #PT_5000 = pressureMax5000
                 #PT_300 = pressureValue300
-                send_data()
+                #send_data()
+                
                 if(PT_5000 >= 2950 and PT_5000 <= 3500):
                     print("PT 5000 OK!")
                 elif (PT_5000 < 0):
@@ -65,30 +68,22 @@ async def run_loop() -> None:
                 else:
                     print("PT 300  OUT OF RANGE!")
                 if(BMS_value < 3.4):
-                    print("Error in BMS values!")
-                    #disconnect battery from motors
+                    print("Error in BMS values!")#disconnect battery from motors
                 else:
                     print("Log all cell values")
-                #if (wheel_encoder < 0.5):
-                    #print("Wheel Encoder OK!")
-                #break_start()
-        
-
+                if (calc < 0.5):
+                    print("Wheel Encoder OK!")
+                
+                GPIO.output(26, GPIO.HIGH)
                 #turn on contactor to begin to check high voltage (contactor, two inverters)
                 #given code to check inverters
 
                 #GUI Indicates that we want to run the pod then:
-                if (GUI_message == 2):
-                    set_current_state(2)
-                    await sio.emit("heard_message", 'heard!', to=sid_save_value)
-                else:
-                    print("GUI has not indicated start still!")
             #STARTING----------------------------------------------------------------
             if(get_current_state() == 2):
                 print("CURRENT STATE 2", get_current_state())
 
                 #SIGNAL THE BRAKES
-                #break_start()
                 #motor_start()
                 #SEND DATA to GUI 
                 if(PT_5000 >= 2950 and PT_5000 <= 3500):
@@ -100,15 +95,15 @@ async def run_loop() -> None:
                 else:
                     print("PT 300  OUT OF RANGE!")
                 send_data()
-                if(brakes_signalled == 1):
-                    print("t\The brakes have been signalled successfully")
+                if(get_break_state() == 1):
+                    print("t\The brakes have been signalled successfully!")
                 else:
-                    print("The brakes were not signalled successfully")
+                    print("The brakes were NOT signalled successfully")
+                set_current_state(3)
 
             #RUNNING--------------------------------------------------------------------
             if(get_current_state() == 3):
-                
-                send_data()
+                #send_data()
                 if(PT_5000 >= 2950 and PT_5000 <= 3500):
                     print("PT 5000 OK!")
                 elif (PT_5000 < 0):
@@ -117,19 +112,15 @@ async def run_loop() -> None:
                     print("PT 300 Checked + in range!")
                 else:
                     print("PT 300  OUT OF RANGE!")
-
-                if (GUI_message == 4):
-                    print("GUI requested STOP")
-                    set_current_state(4)
-
                 if(power_check == 1):
                     print("Power check successful! ")
                 else:
                     print("Power check failed")
-                set_current_state(4)
                 
             #STOPPING-------------------------------------------------------------------------
             if(get_current_state() == 4):
+                
+                GPIO.output(26, GPIO.LOW) 
                 #STOP
                 #contactor_stop()
                 #motor_stop()
@@ -142,10 +133,13 @@ async def run_loop() -> None:
                     print("PT 300 Checked + in range!")
                 else:
                     print("PT 300  OUT OF RANGE!")
-                send_data()
+                #send_data()
                 print("Braking")
+                set_current_state(1)
+                
             #LOADING--------------switches breaks off/on out of fsm, returns to state 1-----
             if(get_current_state() == 5):
+                print("enters loading")
                 if(PT_5000 >= 2950 and PT_5000 <= 3500):
                     print("PT 5000 OK!")
                 elif (PT_5000 < 0):
@@ -165,3 +159,4 @@ async def run_loop() -> None:
             await asyncio.sleep(1)
         except KeyboardInterrupt:
             break
+
